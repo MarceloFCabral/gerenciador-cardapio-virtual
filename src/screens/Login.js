95b84c3db007e240e0ba9927cdfdcc091f294d99
MarceloFCabral <marcelofc12@gmail.com>
2021-03-02 12:42:54 -0300

    testes contexto, rede e utilidades

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,37 +5,43 @@ import {
 } from 'react-native';
 import { CenteredGeneralView } from '../components/views/Views';
 import { LoginImage, Button, TextInput } from '../components/ui/Ui';
+import Context from '../components/Context';
+import * as Network from '../network';
+//import { getBody } from '../utils';
 
 const TelaLogin = () => {
-	const [user, setUser] = React.useState('');
+	const [email, setEmail] = React.useState('');
 	const [pw, setPw] = React.useState('');
+	const [token, setToken] = React.useState('');
 
 	return (
-		<SafeAreaView>
-			<CenteredGeneralView>
-				<LoginImage
-					source={require('../../assets/images/elo-apps.png')}
-				/>
-				<TextInput
-					label="Usuário"
-					value={user}
-					onChangeText={text => setUser(text)}
-					mode="outlined"
-				/>
-				<TextInput
-					label="Senha"
-					value={pw}
-					onChangeText={text => setPw(text)}
-					mode="outlined"
-				/>
-				<Button mode="contained" onPress={() => console.log('login apertado')}>
-					LOGIN
-				</Button>
-				<Text style={{ marginTop: 15 }} onPress={() => console.log("esqueceu a senha")}>
-					Esqueceu a senha?
-				</Text>
-			</CenteredGeneralView>
-		</SafeAreaView>
+		<Context.Provider value={{ token: token }}>
+			<SafeAreaView>
+				<CenteredGeneralView>
+					<LoginImage
+						source={require('../../assets/images/elo-apps.png')}
+					/>
+					<TextInput
+						label="Usuário"
+						value={user}
+						onChangeText={text => setEmail(text)}
+						mode="outlined"
+					/>
+					<TextInput
+						label="Senha"
+						value={pw}
+						onChangeText={text => setPw(text)}
+						mode="outlined"
+					/>
+					<Button mode="contained" onPress={() => Network.login(email, pw)}>
+						LOGIN
+					</Button>
+					<Text style={{ marginTop: 15 }} onPress={() => console.log("esqueceu a senha")}>
+						Esqueceu a senha?
+					</Text>
+				</CenteredGeneralView>
+			</SafeAreaView>
+		</Context.Provider>
 	);
 }
 
